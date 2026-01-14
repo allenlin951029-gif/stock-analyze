36caf578ffee3fcd9ad85b26182f35ef3390c7cc
allenlin951029-gif <allenlin951029@gmail.com>
2026-01-14 19:17:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ with st.sidebar:
         st.caption("尚無歷史紀錄")
     
     st.divider()
-    st.info("")
+
 
 # -------------------------
 # Main
@@ -283,5 +283,6 @@ if archive_len > 0:
 
 else:
     st.info("尚未分析或目前沒有紀錄。請輸入代號並按「開始分析」。")
+
 
 
